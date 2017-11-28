c6ee6a34c2c69bce52eb199e224b16c23dd2efca
Kyle Magilavy <kmagilavy@avantia-inc.com>
2017-11-28 09:55:18 -0500

    Project tests

--- contracts/testProxies/VendorProxy.sol
+++ contracts/testProxies/VendorProxy.sol
@@ -7,4 +7,8 @@ contract VendorProxy is Killable {
     function sendProjectBid(Project project, bytes32 bid) public {
         project.submitBid(bid);
     }
+
+    function revealProjectBid(Project project, uint estimatedCompletionDate, uint estimatedHours, uint hourlyRate) public {
+        project.revealBid(estimatedCompletionDate, estimatedHours, hourlyRate);
+    }
 }
--- test/TestProject.sol
+++ test/TestProject.sol
@@ -8,16 +8,67 @@ import "../contracts/testProxies/VendorProxy.sol";
 
 contract TestProject {
 
+  uint bidEnd = now + 1;
+  uint bidReveal = bidEnd + 1;
+  VendorProxy vendor = new VendorProxy();
+  VendorProxy vendor2 = new VendorProxy();
+  uint estimatedCompletionDate = now + 10000;
+  uint estimatedHours = 500;
+  uint hourlyRate = 300;
+  bytes32 bid = keccak256(estimatedCompletionDate, estimatedHours, hourlyRate);
+
+   struct StatementOfWork {
+        uint estimatedCompletionDate;
+        uint estimatedHours;
+        uint reportedHours;
+        uint hourlyRate;
+        address vendor;
+        bool accepted;
+        bool delivered;
+    }
+
   function testProjectCreation() public {
-    bytes32 bid = "some bid here";
     ProjectRepository repo = ProjectRepository(DeployedAddresses.ProjectRepository());
-    VendorProxy vendor = new VendorProxy();
-
-    repo.createNewProject("new", now + 60, now + 120);
+    
+    repo.createNewProject("new", bidEnd, bidReveal);
+    
     Project myProject = repo.projectAt(0);
-    vendor.sendProjectBid(myProject, bid);
 
-    //mapping(address => bytes32) submittedBids = myProject.submittedBids();
-    Assert.equal(myProject.submittedBids(vendor), bid, "Added bid should match vendor address");
+    Assert.equal(myProject.name(), "new", "Project name should match.");
+  } 
+
+  function testProjectBid() public {
+      ProjectRepository repo = ProjectRepository(DeployedAddresses.ProjectRepository());
+      Project myProject = repo.projectAt(0);
+           
+      vendor.sendProjectBid(myProject, bid);
+      
+      Assert.equal(myProject.submittedBids(vendor), bid, "Added bid should match vendor address");
+  }
+
+  function testAnotherProjectBid() {
+      ProjectRepository repo = ProjectRepository(DeployedAddresses.ProjectRepository());
+      Project myProject = repo.projectAt(0);
+     
+      bytes32 bid2 = "some other bid here";
+      
+      vendor2.sendProjectBid(myProject, bid2);
+      
+      Assert.equal(myProject.submittedBids(vendor2), bid2, "Added bid should match vendor address");
+  }
+
+  function testRevealBid() public {
+     ProjectRepository repo = ProjectRepository(DeployedAddresses.ProjectRepository());
+     Project myProject = repo.projectAt(0);
+
+     while (now < bidEnd) {
+       // wait
+     }
+
+    vendor.revealProjectBid(estimatedCompletionDate, estimatedHours, hourlyRate); // TODO - real values here.
+    
+    //StatementOfWork sow = myProject.revealedBids(vendor);
+       
+    //Assert.equal(sow.estimatedCompletionDate, estimatedCompletionDate, "SOW completion date should match.");
   }
 }
