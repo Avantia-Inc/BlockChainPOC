511ab1c8916e9a152c82c1b5a337767843be6c70
Kyle Magilavy <kmagilavy@avantia-inc.com>
2018-01-22 13:43:48 -0500

    Project create/read

--- contracts/ProjectRepository.sol
+++ contracts/ProjectRepository.sol
@@ -5,17 +5,17 @@ import "./Project.sol";
 contract ProjectRepository {
     mapping(address => Project[]) private projects;
 
-    /*function createNewProject(bytes32 _name, uint _biddingEnd, uint _revealEnd) public {
-        //projects[msg.sender].push(new Project(_name, _biddingEnd, _revealEnd));
+    function createNewProject(bytes32 _name, uint _biddingEnd, uint _revealEnd) public {
+        projects[msg.sender].push(new Project(_name, _biddingEnd, _revealEnd));
         //projects[msg.sender].push(0);
         //new Project(_name, _biddingEnd, _revealEnd);
         // TODO: project creation event
-    }*/
+    }
 
-     function createNewProject(address projectAddress) public {
+    /*function createNewProject(address projectAddress) public {
         Project project = Project(projectAddress);
         projects[msg.sender].push(project);
-    }
+    }*/
 
     // Cannot return dynamic arrays to external callers so this function will only work when called from web3.js
     function myProjects() view public returns (Project[] _myProjects) {
--- src/user/layouts/projects/Projects.js
+++ src/user/layouts/projects/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { loadProjects } from './ProjectsActions'
+import { loadProjects, createProject } from './ProjectsActions'
 
 const loadData = ({loadProjects}) => {
   loadProjects();
@@ -10,18 +10,48 @@ class ProjectsForm extends Component {
   constructor(props) {
     super(props)
 
-    //this.state = {
-      //name: this.props.name
-    //}
+    this.state = {
+      name: this.props.name,
+      endDate: this.props.endDate,
+      revealEndDate: this.props.revealEndDate
+    }
   }
 
   componentDidMount() {
     loadData(this.props);
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onProjectFormSubmit("My Project", new Date(1511203271071), new Date(1511203271071));
+
+    //this.props.onProjectFormSubmit(this.state.name, this.state.endDate, this.state.revealEndDate);
+  }
+
   render() {
     return(
       <main className="container">
+      <form onSubmit={this.handleSubmit.bind(this)}>
+      <fieldset>
+        <legend>Create A New Project</legend>
+        <div>
+          <label>Project Name:</label>
+          <input value={this.state.name} type="text" defaultValue="TEST" id="name" />
+        </div>
+        <div>
+          <label>Bidding End Date</label>
+          <input value={this.state.endDate} type="date" defaultValue="2018-02-09" id="endDate" />
+        </div>
+        <div>
+            <label>Bidding Reveal End Date</label>
+            <input value={this.state.revealEndDate} type="date" defaultValue="2018-02-10" id="revealEndDate" />
+        </div>
+        <div>
+          <button type="submit">Create</button>
+        </div>
+      </fieldset>
+      </form>
       <div className="pure-g">
         <div className="pure-u-1-1">
           <table>
@@ -46,10 +76,18 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onProjectFormSubmit: (name, endDate, revealEndDate) => {
+       event.preventDefault();
+       dispatch(createProject(name, endDate, revealEndDate));
+    },
+    loadProjects: loadProjects
+  }
+}
+
 export default connect(
   mapStateToProps,
-  {
-    loadProjects
-  }
+  mapDispatchToProps
 )(ProjectsForm);
 
